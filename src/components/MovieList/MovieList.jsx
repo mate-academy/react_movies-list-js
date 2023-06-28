a1b95177388ca7c543a4ebee9ace03461364b95f
Pavlo Kuraksin <kuraksinpavel@gmail.com>
2023-06-28 20:36:58 +0300

    add few  fixes based on rewiev

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,12 +1,8 @@
 import './MovieList.scss';
-import {MovieCard} from "../MovieCard";
+import { MovieCard } from '../MovieCard';
 
 export const MovieList = ({ movies }) => (
-  <>
-    <div className="movies">
-      {movies.map(movie =>
-        <MovieCard key={movie.imdbId} movie={movie}/>
-      )}
-    </div>
-  </>
+  <div className="movies">
+    {movies.map(movie => <MovieCard key={movie.imdbId} movie={movie} />)}
+  </div>
 );
