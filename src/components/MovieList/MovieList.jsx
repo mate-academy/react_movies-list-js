8d4e31c9de75dd067bba69214311a7b8b020880f
Oleksandr Kulibaba <alks.aesthetic@gmail.com>
2023-06-29 20:07:32 +0300

    fixes after review

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -2,11 +2,9 @@ import { MovieCard } from '../MovieCard';
 import './MovieList.scss';
 
 export const MovieList = ({ movies }) => (
-  <>
-    <div className="movies">
-      {movies.map(movie => (
-        <MovieCard key={movie.imdbId} movie={movie} />
-      ))}
-    </div>
-  </>
+  <ul className="movies">
+    {movies.map(movie => (
+      <MovieCard movie={movie} key={movie.imdbId} />
+    ))}
+  </ul>
 );
