7d62a7ce609e8f1f0816502b9df3182d6e204fdb
Kateryna Kylyvnyk <kkylyvnyk@gmail.com>
2023-06-29 16:50:36 +0300

    Solution

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,12 @@ import movies from './api/movies.json';
 import { MovieList } from './components/MovieList';
 
 export const App = () => (
-  <>
-    <div className="page">
-      <div className="page-content">
-        <MovieList movies={movies} />
-      </div>
+  <div className="page">
+    <div className="page-content">
+      <MovieList movies={movies} />
     </div>
     <div className="sidebar" data-cy="Sidebar">
       Sidebar will be here
     </div>
-  </>
+  </div>
 );
