033ec029d484e973ec492a12df6283b00899d10a
Kateryna Kylyvnyk <kkylyvnyk@gmail.com>
2023-06-29 16:54:56 +0300

    Solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-/* eslint-disable max-len */
-
 import './App.scss';
 import movies from './api/movies.json';
 import { MovieList } from './components/MovieList';
 
 export const App = () => (
-  <div className="page">
-    <div className="page-content">
-      <MovieList movies={movies} />
+  <>
+    <div className="page">
+      <div className="page-content">
+        <MovieList movies={movies} />
+      </div>
+      <div className="sidebar" data-cy="Sidebar">
+        Sidebar will be here
+      </div>
     </div>
-    <div className="sidebar" data-cy="Sidebar">
-      Sidebar will be here
-    </div>
-  </div>
+  </>
 );
