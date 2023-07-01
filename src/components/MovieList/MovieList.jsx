cfa73eff8a201a8d2055306c0922fc0fc5fe4817
Absolution26 <zet2571428@gmail.com>
2023-07-02 01:20:42 +0300

    delete unnecessary wraps

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -2,11 +2,9 @@ import { MovieCard } from '../MovieCard';
 import './MovieList.scss';
 
 export const MovieList = ({ movies }) => (
-  <>
-    <div className="movies">
-      { movies.map(movieItem => (
-        <MovieCard movie={movieItem} key={movieItem.imdbId} />
-      ))}
-    </div>
-  </>
+  <div className="movies">
+    { movies.map(movieItem => (
+      <MovieCard movie={movieItem} key={movieItem.imdbId} />
+    ))}
+  </div>
 );
