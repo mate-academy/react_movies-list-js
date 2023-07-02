029a60219937681c0fd382121ba7090687403db5
Pavlo Kuraksin <kuraksinpavel@gmail.com>
2023-07-02 22:21:09 +0300

    add fixes

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,8 +1,13 @@
 import './MovieList.scss';
-import { MovieCard } from '../MovieCard';
+import {MovieCard} from '../MovieCard';
 
 export const MovieList = ({ movies }) => (
   <div className="movies">
-    {movies.map(movie => <MovieCard key={movie.imdbId} movie={movie} />)}
+    {movies.map(movie => (
+      <MovieCard
+        key={movie.imdbId}
+        movie={movie}
+      />
+    ))}
   </div>
 );
