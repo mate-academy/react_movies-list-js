80730ec2d79843ac2c77db7bc1b6a0be91139d19
Skrudj <ssinilov@gmail.com>
2023-07-04 13:49:56 +0300

    fix code style issues in MovieCard component

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,14 +1,16 @@
 import './MovieCard.scss';
 
-export const MovieCard = ({ movie }) => (
-  <>
+export const MovieCard = ({ movie }) => {
+  const { title, description, imgUrl, imdbUrl } = movie;
+
+  return (
     <div className="card" data-cy="Movie">
       <div className="card-image">
         <figure className="image is-4by3">
           <img
             data-cy="MovieImage"
-            src={movie.imgUrl}
-            alt="Film logo"
+            src={imgUrl}
+            alt="MovieImage"
           />
         </figure>
       </div>
@@ -23,18 +25,18 @@ export const MovieCard = ({ movie }) => (
 
           <div className="media-content">
             <p className="title is-8" data-cy="MovieTitle">
-              {movie.title}
+              {title}
             </p>
           </div>
         </div>
 
         <div className="content">
           <p data-cy="MovieDescription">
-            {movie.description}
+            {description}
           </p>
 
           <a
-            href={movie.imdbUrl}
+            href={imdbUrl}
             data-cy="MovieLink"
           >
             IMDB
@@ -42,5 +44,5 @@ export const MovieCard = ({ movie }) => (
         </div>
       </div>
     </div>
-  </>
-);
+  );
+};
