112106996dd66755bf31f8658a1ab31c1935e6df
Kriv <kad180198@gmail.com>
2023-07-04 18:45:54 +0300

    add some change

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -2,15 +2,11 @@ import { MovieCard } from '../MovieCard';
 import './MovieList.scss';
 
 export const MovieList = ({ movies }) => (
-  <>
-    <div className="movies">
-      <ul>
-        {movies.map(movie => (
-          <li key={movie.imdbId}>
-            <MovieCard movie={movie} />
-          </li>
-        ))}
-      </ul>
-    </div>
-  </>
+  <ul className="movies">
+    {movies.map(movie => (
+      <li key={movie.imdbId}>
+        <MovieCard movie={movie} />
+      </li>
+    ))}
+  </ul>
 );
