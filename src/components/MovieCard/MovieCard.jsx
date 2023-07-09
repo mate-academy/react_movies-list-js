49e760ca883d29cc37c933313e348b1b88703183
Ilia Osoben <osoben17@gmail.com>
2023-07-09 18:16:49 +0300

    fixed

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,42 +1,39 @@
 import './MovieCard.scss';
 
-export const MovieCard = ({ movie }) => (
-  <div className="card" data-cy="Movie">
-    <div className="card-image">
-      <figure className="image is-4by3">
-        <img
-          data-cy="MovieImage"
-          src={movie.imgUrl}
-          alt="Film logo"
-        />
-      </figure>
-    </div>
+export const MovieCard = ({ movie }) => {
+  const { imgUrl, title, description, imdbUrl } = movie;
 
-    <div className="card-content">
-      <div className="media">
-        <div className="media-left">
-          <figure className="image is-48x48">
-            <img src={movie.imgUrl} alt="imdb" />
-          </figure>
-        </div>
-
-        <div className="media-content">
-          <p className="title is-8" data-cy="MovieTitle">
-            {movie.title}
-          </p>
-        </div>
+  return (
+    <div className="card" data-cy="Movie">
+      <div className="card-image">
+        <figure className="image is-4by3">
+          <img data-cy="MovieImage" src={imgUrl} alt="Film logo" />
+        </figure>
       </div>
 
-      <div className="content">
-        <p data-cy="MovieDescription">
-          {movie.description}
+      <div className="card-content">
+        <div className="media">
+          <div className="media-left">
+            <figure className="image is-48x48">
+              <img src="images/imdb-logo.jpeg" alt="imdb" />
+            </figure>
+          </div>
 
-        </p>
+          <div className="media-content">
+            <p className="title is-8" data-cy="MovieTitle">
+              {title}
+            </p>
+          </div>
+        </div>
 
-        <a href={movie.imdbUrl} data-cy="MovieLink">
-          IMDB
-        </a>
+        <div className="content">
+          <p data-cy="MovieDescription">{description}</p>
+
+          <a href={imdbUrl} data-cy="MovieLink">
+            IMDB
+          </a>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
