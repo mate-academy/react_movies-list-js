5c65ce8a38ccdfb6bcab49f827e4775798c4532a
Misha Siv <0Sivcov0@gmail.com>
2023-07-11 19:12:51 +0300

    add search bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,51 @@
+import { useState } from 'react';
 import './App.scss';
 
 import moviesFromServer from './api/movies.json';
 import { MovieList } from './components/MovieList';
 
+function getMovie(movies, query) {
+  const searchQuery = query.trim().toLowerCase();
 
-export const App = () => (
-  <div className="page">
-    <div className="page-content">
-      <MovieList movies={moviesFromServer} />
-    </div>
+  if (searchQuery.length > 0) {
+    return movies.filter(el => el.title.toLowerCase().includes(searchQuery)
+      || el.description.toLowerCase().includes(searchQuery));
+  }
 
-    <div className="sidebar" data-cy="Sidebar">
-      Sidebar will be here
+  return movies;
+}
+
+export const App = () => {
+  const [query, setQuery] = useState('');
+  const visibleMovies = getMovie(moviesFromServer, query);
+
+  return (
+    <div className="page">
+      <div className="page-content">
+        <div className="box">
+          <div className="field">
+            <label htmlFor="search-qery" className="label">
+              Search movie
+            </label>
+
+            <div className="control">
+              <input
+                type="text"
+                id="search-qery"
+                className="input"
+                placeholder="Type search word"
+                onChange={event => setQuery(event.target.value)}
+              />
+            </div>
+          </div>
+        </div>
+
+        <MovieList movies={visibleMovies} />
+      </div>
+
+      <div className="sidebar" data-cy="Sidebar">
+        Sidebar will be here
+      </div>
     </div>
-  </div>
-);
+  );
+};
