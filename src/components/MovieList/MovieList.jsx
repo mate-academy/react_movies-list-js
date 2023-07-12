7000087393553202d9401ecb0f81f7299a7cd359
Vladislav Tsymbalist <vladislavtsimba1999@gmail.com>
2023-07-12 14:10:09 +0300

    deleted react fragment,moved key

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -2,11 +2,9 @@ import './MovieList.scss';
 import { MovieCard } from '../MovieCard';
 
 export const MovieList = ({ movies }) => (
-  <>
-    <div className="movies">
-      {movies.map(movie => (
-        <MovieCard movie={movie} />
-      ))}
-    </div>
-  </>
+  <div className="movies">
+    {movies.map(movie => (
+      <MovieCard movie={movie} key={movie.imdbId} />
+    ))}
+  </div>
 );
