6df63f92f0e8e72d0271bcaad65f51ebd37323f4
david.shamiiev <david.shamiiev@rad4m.com>
2023-07-13 13:18:55 +0300

    solution

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,5 +1,17 @@
+/* eslint-disable max-len */
+import { MovieCard } from '../MovieCard';
 import './MovieList.scss';
 
-export const MovieList = () => (
-  <>Put the list here</>
+export const MovieList = ({ movies }) => (
+  <div className="movies">
+    {movies.map(({ title, description, imgUrl, imdbUrl, imdbId }) => (
+      <MovieCard
+        key={imdbId}
+        title={title}
+        description={description}
+        imgUrl={imgUrl}
+        imdbUrl={imdbUrl}
+      />
+    ))}
+  </div>
 );
