93f5d2228c885f7a24f1e4291b128aea55f6326c
Andrii Khomiak <andr.hamster@gmail.com>
2023-07-13 16:40:35 +0300

    task done

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,5 +1,13 @@
 import './MovieList.scss';
+import { MovieCard } from '../MovieCard';
 
-export const MovieList = () => (
-  <>Put the list here</>
+export const MovieList = ({ movies }) => (
+  <ul className="movies">
+    {movies.map(movie => (
+      <MovieCard
+        movie={movie}
+        key={movie.imdbId}
+      />
+    ))}
+  </ul>
 );
