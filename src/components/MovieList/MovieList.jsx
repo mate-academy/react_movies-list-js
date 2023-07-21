0645f4151547dd21c2c6f3fa285143f9e84bf322
purplefade <tarovykanton@gmail.com>
2023-07-21 21:57:58 +0300

    add solution

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,5 +1,18 @@
 import './MovieList.scss';
+import { MovieCard } from '../MovieCard';
 
-export const MovieList = () => (
-  <>Put the list here</>
-);
+export const MovieList = ({ movies }) => {
+  const moviesExist = movies.length > 0;
+
+  return (
+    <div className="movies">
+      {moviesExist ? (
+        movies.map(movie => (
+          <MovieCard movie={movie} key={movie.imdbId} />
+        ))
+      ) : (
+        <div>No movies available</div>
+      )}
+    </div>
+  );
+};
