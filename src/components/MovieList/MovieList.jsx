7ba12e6c0f23c0c273b0f8998dfbb3bb7cb5b336
Dmytro Tymchenko <zav.pro_kr@ukr.net>
2023-07-21 17:34:40 +0300

    MoviesList

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,5 +1,15 @@
+import { MovieCard } from '../MovieCard';
 import './MovieList.scss';
 
-export const MovieList = () => (
-  <>Put the list here</>
+export const MovieList = ({ movies }) => (
+  <ul
+    className="movies"
+    data-cy="MovieTitle"
+  >
+    {movies.map(movie => (
+      <li key={movie.imdbId}>
+        <MovieCard movie={movie} />
+      </li>
+    ))}
+  </ul>
 );
