030d263094cc5ab22d22ed6fde138066d77c05f3
Nataliia <koreniuknataliia@gmail.com>
2023-07-21 22:12:19 +0300

    Fixed component name

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,7 +1,7 @@
 import { MovieCard } from '../MovieCard';
 import './MovieList.scss';
 
-export const MoviesList = ({ movies }) => (
+export const MovieList = ({ movies }) => (
   <div className="movies">
     {movies.map(movie => (
       <MovieCard movie={movie} key={movie.imdbId} />
