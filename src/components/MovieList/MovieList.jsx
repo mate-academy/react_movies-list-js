3c19934b1afbaba922411dba5aaae7c39c37def9
Volodymyr Shostak <shostak_1@proton.me>
2023-07-21 22:09:59 +0300

    movies list

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -3,11 +3,10 @@ import './MovieList.scss';
 
 export const MovieList = ({ movies }) => (
   <div className="movies">
-    {movies.length > 0
-      && movies.map(movie => (
-        <div className="card" data-cy="Movie" key={movie.imdbId}>
-          <MovieCard movie={movie} />
-        </div>
-      ))}
+    {movies.map(movie => (
+      <div className="card" data-cy="Movie" key={movie.imdbId}>
+        <MovieCard movie={movie} />
+      </div>
+    ))}
   </div>
 );
