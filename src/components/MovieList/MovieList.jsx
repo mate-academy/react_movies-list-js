bda77837487e443af84ea99c7316dfde59fa3ece
Bzhezinskyi Stanislav <bzhezinskyi_stanislav@Bzhezinskyis-MacBook-Air.local>
2023-07-31 13:34:21 +0300

    React task solution

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,5 +1,16 @@
 import './MovieList.scss';
+import { MovieCard } from '../MovieCard/MovieCard';
 
-export const MovieList = () => (
-  <>Put the list here</>
+export const MovieList = ({ movies }) => (
+  <div className="movies">
+    {movies.map(movie => (
+      <MovieCard
+        title={movie.title}
+        description={movie.description}
+        imgUrl={movie.imgUrl}
+        imdbUrl={movie.imdbUrl}
+        key={movie.imdbId}
+      />
+    ))}
+  </div>
 );
