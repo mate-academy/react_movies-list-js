44d23f311511b622e374a499cfcfdbdb7510f5e5
Iryna Polchaninova <ira.polchanin@gmail.com>
2023-07-31 09:52:52 +0300

    first commit

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,5 +1,10 @@
+import { MovieCard } from '../MovieCard';
 import './MovieList.scss';
 
-export const MovieList = () => (
-  <>Put the list here</>
+export const MovieList = ({ moviesFromServer }) => (
+  <div className="movies">
+    {moviesFromServer && moviesFromServer.map(movie => (
+      <MovieCard movie={movie} key={movie.imdbId} />
+    ))}
+  </div>
 );
