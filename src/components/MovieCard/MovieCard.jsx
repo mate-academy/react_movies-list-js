c4025480f1027c7d2c5643d86ef19e2c3c06d16d
Hanna-Balabukha <anna.aniskovets1992@ukr.net>
2023-08-03 15:50:24 +0300

    'styles'

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,48 +1,56 @@
 import './MovieCard.scss';
 
-export const MovieCard = ({ movie }) => (
-  <div
-    className="MovieCard"
-  >
-    <div className="card" data-cy="Movie">
-      <div className="card-image">
-        <figure className="image is-4by3">
-          <img
-            data-cy="MovieImage"
-            src={movie.imgUrl}
-            alt="Film logo"
-          />
-        </figure>
-      </div>
+export const MovieCard = ({ movie }) => {
+  const {
+    imgUrl,
+    title,
+    description,
+  } = movie;
 
-      <div className="card-content">
-        <div className="media">
-          <div className="media-left">
-            <figure className="image is-48x48">
-              <img src="images/imdb-logo.jpeg" alt="imdb" />
-            </figure>
-          </div>
-
-          <div className="media-content">
-            <p className="title is-8" data-cy="MovieTitle">
-              {movie.title}
-            </p>
-          </div>
+  return (
+    <div
+      className="MovieCard"
+    >
+      <div className="card" data-cy="Movie">
+        <div className="card-image">
+          <figure className="image is-4by3">
+            <img
+              data-cy="MovieImage"
+              src={imgUrl}
+              alt="Film logo"
+            />
+          </figure>
         </div>
 
-        <div className="content">
-          <p data-cy="MovieDescription">
-            {movie.description}
-          </p>
+        <div className="card-content">
+          <div className="media">
+            <div className="media-left">
+              <figure className="image is-48x48">
+                <img src="images/imdb-logo.jpeg" alt="imdb" />
+              </figure>
+            </div>
 
-          <a
-            href="https://www.imdb.com/title/tt1375666"
-            data-cy="MovieLink"
-          >
-            {movie.imdbUrl}
-          </a>
+            <div className="media-content">
+              <p className="title is-8" data-cy="MovieTitle">
+                {title}
+              </p>
+            </div>
+          </div>
+
+          <div className="content">
+            <p data-cy="MovieDescription">
+              {description}
+            </p>
+
+            <a
+              href="https://www.imdb.com/title/tt1375666"
+              data-cy="MovieLink"
+            >
+              IMDB
+            </a>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
