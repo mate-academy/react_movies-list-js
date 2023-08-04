61eccb0a7197f7a5321adee585d214218c0102b4
Yuliia-Hrinchenko1 <yulya9801@gmail.com>
2023-08-04 21:11:52 +0300

    Code was improved

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,14 +1,14 @@
 /* eslint-disable max-len */
 import './MovieCard.scss';
 
-export const MovieCard = ({ movie }) => (
-  <div className="card" data-cy="Movie">
+export const MovieCard = ({ movie: { title, description, imgUrl, imdbUrl, imdbId } }) => (
+  <li className="card" data-cy="Movie">
     <div className="card-image">
       <figure className="image is-4by3">
         <img
           data-cy="MovieImage"
-          src={movie.imgUrl}
-          alt={movie.title}
+          src={imgUrl}
+          alt={title}
         />
       </figure>
     </div>
@@ -23,23 +23,23 @@ export const MovieCard = ({ movie }) => (
 
         <div className="media-content">
           <p className="title is-8" data-cy="MovieTitle">
-            {movie.title}
+            {title}
           </p>
         </div>
       </div>
 
       <div className="content">
         <p data-cy="MovieDescription">
-          {movie.description}
+          {description}
         </p>
 
         <a
-          href={movie.imdbUrl}
+          href={imdbUrl}
           data-cy="MovieLink"
         >
           IMDB
         </a>
       </div>
     </div>
-  </div>
+  </li>
 );
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -2,9 +2,9 @@ import './MovieList.scss';
 import { MovieCard } from '../MovieCard';
 
 export const MovieList = ({ movies }) => (
-  <div className="MovieList">
+  <ul className="MovieList">
     {movies.map(movie => (
       <MovieCard movie={movie} key={movie.imdbId} />
     ))}
-  </div>
+  </ul>
 );
