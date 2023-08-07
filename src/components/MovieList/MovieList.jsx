9cebffda32b2fe53dc8c1328f17eed13116e2b11
Levi199601 <udovichenkoivan16@gmail.com>
2023-08-07 15:58:13 -0700

    delete div

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -3,9 +3,9 @@ import moviesFromServer from '../../api/movies.json';
 import { MovieCard } from '../MovieCard';
 
 export const MovieList = () => (
-  <div>
+  <>
     {moviesFromServer.map(movie => (
       <MovieCard movie={movie} key={movie.imdbId} />
     ))}
-  </div>
+  </>
 );
