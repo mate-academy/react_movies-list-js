6f6d06b3ed441bd51d11633730dca64c24c63912
Andriy Monko <Andriy17monko@gmail.com>
2023-08-09 12:42:58 +0300

    practical task 4

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,5 +1,26 @@
 import './MovieCard.scss';
 
-export const MovieCard = () => (
-  <>Put the card here</>
+export const MovieCard = ({ movies }) => (
+  <div className="Card">
+    <div className="card-image">
+      <img
+        className="MoviesPoster"
+        data-cy="MovieImage"
+        alt="Film logo"
+        src={movies.imgUrl}
+      />
+    </div>
+
+    <div className="Film_container">
+      <img src="images/imdb-logo.jpeg" alt="imdb" className="IMDB_Photo" />
+
+      <p className="FilmTitle">{movies.title}</p>
+    </div>
+
+    <div className="MovieDescription">{movies.description}</div>
+
+    <p>
+      <a href={movies.imdbUrl}>IMDB</a>
+    </p>
+  </div>
 );
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,5 +1,10 @@
+import { MovieCard } from '../MovieCard';
 import './MovieList.scss';
 
-export const MovieList = () => (
-  <>Put the list here</>
+export const MovieList = ({ moviesFromServer }) => (
+  <div className="Movies">
+    {moviesFromServer.map(movies => (
+      <MovieCard movies={movies} key={movies.imdbId} />
+    ))}
+  </div>
 );
