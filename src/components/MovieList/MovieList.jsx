f10fbb14fef6057948747adb91e62fd0932145d6
Kava Bohdan <bogdan.kava.99@gmail.com>
2023-08-23 00:34:25 +0300

    add destructuring

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,10 +1,14 @@
 import { MovieCard } from '../MovieCard';
 import './MovieList.scss';
 
-export const MovieList = ({ movies }) => (
-  <div className="movies">
-    {movies.map(movie => (
-      <MovieCard movie={movie} key={movie.imdbId} />
-    ))}
-  </div>
-);
+export const MovieList = ({ movies }) => {
+  const { imdbId } = movies;
+
+  return (
+    <div className="movies">
+      {movies.map(movie => (
+        <MovieCard movie={movie} key={imdbId} />
+      ))}
+    </div>
+  );
+};
