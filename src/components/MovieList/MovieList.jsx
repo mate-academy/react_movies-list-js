c8d604f3c031dacf53ea5b897b140c99dc12320a
Denys Poltorak <itwfrontend@gmail.com>
2023-08-22 13:25:30 +0300

    feat: add task solution

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,5 +1,12 @@
+import { MovieCard } from '../MovieCard';
+import moviesFromServer from '../../api/movies.json';
+
 import './MovieList.scss';
 
 export const MovieList = () => (
-  <>Put the list here</>
+  <div className="movies">
+    {moviesFromServer.map(movie => (
+      <MovieCard key={movie.imdbId} movie={movie} />
+    ))}
+  </div>
 );
