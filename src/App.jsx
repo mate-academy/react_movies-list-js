f97e64a7663a5ba8d21d3b1caee8a2df6eaf7267
Olha Momot <helgammt@gmail.com>
2023-08-23 16:19:51 +0300

    add destructuring

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import './App.scss';
-import movies from './api/movies.json';
+import moviesFromServer from './api/movies.json';
 import { MovieList } from './components/MovieList';
 
 export const App = () => (
   <div className="page">
     <div className="page-content">
-      <MovieList movies={movies} />
+      <MovieList movies={moviesFromServer} />
     </div>
 
     <div className="sidebar" data-cy="Sidebar">
