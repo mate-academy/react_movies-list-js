ebece5d1dd9d405f0af830324ba41aafa693dce4
Oleh0293 <Oleh.kulyk.ik.2021@lpnu.ua>
2023-08-23 08:46:58 +0300

    Corected mistakes

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import { MovieList } from './components/MovieList/MovieList';
 export const App = () => (
   <div className="page">
     <div className="page-content">
-      <MovieList movies={movies} key={movies.imdbId} />
+      <MovieList movies={movies} />
     </div>
 
     <div className="sidebar" data-cy="Sidebar">
