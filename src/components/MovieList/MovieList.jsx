4eb43f303f077d143f8725a92acf63579c67127e
Yevhenii Voronin <voronine@ukr.net>
2023-08-23 15:38:29 +0300

    add task solution

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,5 +1,10 @@
 import './MovieList.scss';
+import { MovieCard } from '../MovieCard';
 
-export const MovieList = () => (
-  <>Put the list here</>
-);
+export const Movelist = ({ movies }) => {
+  <div className="movies">
+    {movies.map(movie => (
+      <MovieCard movies={movie} key={movie.imdbid} />
+    ))}
+  </div>;
+};
