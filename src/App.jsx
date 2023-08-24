548ed371464fcf3dc2df5d39f245de1cc90062ee
KlymSudakov <sudakov.klym@gmail.com>
2023-08-25 00:20:11 +0300

    add fixes

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,7 @@ import movies from './api/movies.json';
 export const App = () => (
   <div className="page">
     <div className="page-content">
-      <MovieList
-        movies={movies}
-      />
+      <MovieList movies={movies} />
     </div>
 
     <div className="sidebar" data-cy="Sidebar">
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -4,10 +4,7 @@ import { MovieCard } from '../MovieCard';
 export const MovieList = ({ movies }) => (
   <div className="movies">
     {movies.map(movie => (
-      <MovieCard
-        movie={movie}
-        key={movie.imdbId}
-      />
+      <MovieCard movie={movie} key={movie.imdbId} />
     ))}
   </div>
 );
