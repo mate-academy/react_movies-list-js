7857c557c21ccb3ee7639f85c36aedab5eaaa473
KatOlista^^! <olya.katyushina@gmail.com>
2023-08-24 17:08:06 +0200

    add sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.scss';
 
 import moviesFromServer from './api/movies.json';
 import { MovieList } from './components/MovieList';
+import { Sidebar } from './components/Sidebar';
 
 export const App = () => (
   <div className="page">
@@ -9,8 +10,6 @@ export const App = () => (
       <MovieList movies={moviesFromServer} />
     </div>
 
-    <div className="sidebar" data-cy="Sidebar">
-      Sidebar will be here
-    </div>
+    <Sidebar />
   </div>
 );
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -4,7 +4,8 @@ import { MovieCard } from '../MovieCard';
 
 export const MovieList = ({ movies }) => (
   <div className="movies">
-    {movies
-      .map(movie => <MovieCard movie={movie} key={movie.imdbId} />)}
+    {movies.map(movie => (
+      <MovieCard movie={movie} key={movie.imdbId} />
+    ))}
   </div>
 );
