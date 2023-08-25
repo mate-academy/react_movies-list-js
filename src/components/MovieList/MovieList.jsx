1c76535b91be05e6d88aafcdc1da76a40c72cf1f
Ivanna Romanovych <44444911+ivannaromanovych@users.noreply.github.com>
2023-08-25 11:16:11 +0300

    minor fixes

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -4,7 +4,7 @@ import './MovieList.scss';
 export const MovieList = ({ movies }) => (
   <div className="movies">
     {movies.map(movie => (
-      <MovieCard key={movie.key} movie={movie} />
+      <MovieCard key={movie.imdbId} movie={movie} />
     ))}
   </div>
 );
