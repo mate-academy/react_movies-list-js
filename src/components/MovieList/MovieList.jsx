f544ce8e5d9c1ce7c8d41489dcd2828965f31269
irinagos <irynagos47@gmail.com>
2023-08-25 10:02:15 +0300

    fixed code

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -3,11 +3,9 @@ import './MovieList.scss';
 import { MovieCard } from '../MovieCard/MovieCard';
 
 export const MovieList = ({ movies }) => (
-  movies.length !== 0 && (
-    <div className="movies">
-      {movies.map(movie => (
-        <MovieCard movie={movie} key={movie.imdbId} />
-      ))}
-    </div>
-  )
+  <div className="movies">
+    {movies.map(movie => (
+      <MovieCard movie={movie} key={movie.imdbId} />
+    ))}
+  </div>
 );
