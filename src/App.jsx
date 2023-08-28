a92809ed30de6767719ec8e7f72a98606a683da6
Ivanna Romanovych <ivankaromanovych12@gmail.com>
2023-08-28 12:30:05 +0300

    separate sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@
 import './App.scss';
 import moviesFromServer from './api/movies.json';
 import { MovieList } from './components/MovieList';
+import { Sidebar } from './components/Sidebar';
 
 export const App = () => (
   <div className="page">
@@ -10,8 +11,6 @@ export const App = () => (
       <MovieList movies={moviesFromServer} />
     </div>
 
-    <div className="sidebar" data-cy="Sidebar">
-      Sidebar will be here
-    </div>
+    <Sidebar />
   </div>
 );
