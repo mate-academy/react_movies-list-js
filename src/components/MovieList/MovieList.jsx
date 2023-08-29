ba7d7d711edc88ab1355322502c033ba8724e132
Oleksandr Shevchenko <aleksandrtarasovi44@gmail.com>
2023-08-29 17:59:40 +0300

    add task solution

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -2,9 +2,9 @@ import './MovieList.scss';
 import { MovieCard } from '../MovieCard/MovieCard';
 
 export const MovieList = ({ movies }) => (
-  <ul>
+  <section className="movies">
     {movies.map(movie => (
       <MovieCard key={movie.imdbId} movie={movie} />
     ))}
-  </ul>
+  </section>
 );
