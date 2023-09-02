2de930c5233ce613128ff4befd41111b05e3cc49
Ivan Baranovskyi <baranovskyiivan@gmail.com>
2023-09-02 11:36:27 +0200

    fixed position of side bar

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,10 @@ export const App = () => (
   <div className="page">
     <div className="page-content">
       <MovieList movies={moviesFromServer} />
+    </div>
 
-      <div className="sidebar" data-cy="Sidebar">
-        Sidebar will be here
-      </div>
-
+    <div className="sidebar" data-cy="Sidebar">
+      Sidebar will be here
     </div>
   </div>
 );
