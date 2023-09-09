903100013a7765203f7033815b9a7665b8c00be3
Vadym Bavorovskyi <vadymbavorovskiy@gmail.com>
2023-09-09 12:30:48 +0300

    fix problem

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,12 +1,14 @@
 import './MovieList.scss';
 import { MovieCard } from '../MovieCard';
 
-export const MovieList = ({ moviesFromServer }) => (
-  <div className="movies">
-    { moviesFromServer.length !== 0 && (
-      moviesFromServer.map(movie => (
-        <MovieCard key={movie.imdbId} movie={movie} />
-      )))
-    }
+export const MovieList = ({ movies }) => (
+  <div className="page-content">
+    <div className="movies">
+      {
+        movies && movies.map(movie => (
+          <MovieCard key={movie.imdbId} movie={movie} />
+        ))
+      }
+    </div>
   </div>
 );
