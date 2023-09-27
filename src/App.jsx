d3526409f3fd00ccd6035a9dc5fc27c6132d2b04
Roma <roma150269@gmail.com>
2023-09-27 12:27:01 +0300

    Solution

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { MovieList } from './components/MovieList/MovieList';
 export const App = () => (
   <div className="page">
     <div className="page-content">
-      <MovieList moviesFromServer={moviesFromServer} />
+      <MovieList movies={moviesFromServer} />
     </div>
     <div className="sidebar" data-cy="Sidebar">
       Sidebar will be here
