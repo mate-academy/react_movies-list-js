1f0526d399e6a076453cb13982e11f1bd6d4b859
dmytronykorovych <dmytro.nykorovych@gmail.com>
2023-09-27 17:13:04 +0200

    Fix error

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { MovieList } from './components/MovieList';
 export const App = () => (
   <div className="page">
     <div className="page-content">
-      <MovieList movieList={moviesFromServer} />
+      <MovieList movies={moviesFromServer} />
     </div>
 
     <div className="sidebar" data-cy="Sidebar">
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,14 +1,12 @@
 import { MovieCard } from '../MovieCard';
 import './MovieList.scss';
 
-export const MovieList = ({ movieList }) => (
-  <>
-    {movieList ? (
-      <div className="movies">
-        {movieList.map(movie => (
-          <MovieCard movie={movie} key={movie.imdbId} />
-        ))}
-      </div>
-    ) : ''}
-  </>
+export const MovieList = ({ movies }) => (
+  movies ? (
+    <div className="movies">
+      {movies.map(movie => (
+        <MovieCard movie={movie} key={movie.imdbId} />
+      ))}
+    </div>
+  ) : ''
 );
