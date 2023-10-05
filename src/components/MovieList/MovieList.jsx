9b5e6c58b72250840fe14b1c31b67c24ea58de04
Vika Levik <levika002@gmail.com>
2023-10-05 15:42:30 +0300

    created movie list

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,5 +1,16 @@
 import './MovieList.scss';
+import { MovieCard } from '../MovieCard';
 
-export const MovieList = () => (
-  <>Put the list here</>
+export const MovieList = ({ movies }) => (
+  <>
+    {movies && movies.length && (
+      <div className="movies">
+        {movies.map(movie => (
+          <MovieCard movie={movie} />
+        ))
+        }
+      </div>
+    )
+    }
+  </>
 );
