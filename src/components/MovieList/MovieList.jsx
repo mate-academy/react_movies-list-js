92b7abef1dc9eac0d6fbfe8eec1513c0e925f5f3
Albert Kifiuk <albertkifiuk@gmail.com>
2023-10-14 15:02:02 +0300

    add task solution

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -4,7 +4,7 @@ import './MovieList.scss';
 export const MovieList = ({ moviesFromServer }) => (
   <div className="movies">
     {moviesFromServer?.map(movie => (
-      <MovieCard movie={movie} key={movie.imdbId} />
+      <MovieCard movie={movie} key={movie.imdbId} data-cy="Movie" />
     ))}
   </div>
 );
