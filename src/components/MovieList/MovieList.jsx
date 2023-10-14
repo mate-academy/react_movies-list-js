821bd2d8af8a2d76db742fc3da267ebdd4e18d14
Albert Kifiuk <albertkifiuk@gmail.com>
2023-10-14 15:27:00 +0300

    add task solution

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,16 +1,10 @@
 import { MovieCard } from '../MovieCard';
 import './MovieList.scss';
 
-export const MovieList = ({ moviesFromServer }) => {
-  // Сортировка фильмов по imdbId
-  const sortedMovies = [...moviesFromServer]
-    .sort((a, b) => a.imdbId.localeCompare(b.imdbId));
-
-  return (
-    <div className="movies">
-      {sortedMovies.map(movie => (
-        <MovieCard movie={movie} key={movie.imdbId} />
-      ))}
-    </div>
-  );
-};
+export const MovieList = ({ moviesFromServer }) => (
+  <div className="movies">
+    {moviesFromServer?.map(movie => (
+      <MovieCard movie={movie} key={movie.imdbId} />
+    ))}
+  </div>
+);
