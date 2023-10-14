6f602b6ad68a933bf526638fe87c56e1f3d2af5e
Albert Kifiuk <albertkifiuk@gmail.com>
2023-10-14 16:16:52 +0300

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { MovieList } from './components/MovieList';
 export const App = () => (
   <div className="page">
     <div className="page-content">
-      <MovieList moviesFromServer={moviesFromServer} />
+      <MovieList movies={moviesFromServer} />
     </div>
 
     <div className="sidebar" data-cy="Sidebar">
