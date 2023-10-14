20cd8273dfaaf78a6bb3f0d6ae0db8a9b52a0bcd
Albert Kifiuk <albertkifiuk@gmail.com>
2023-10-14 14:34:48 +0300

    add task solution

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,11 +1,22 @@
 import { MovieCard } from '../MovieCard';
 import './MovieList.scss';
 
-export const MovieList = ({ moviesFromServer }) => (
-  <div className="movies">
-    {moviesFromServer?.map(movies => (
-      <MovieCard movie={movies} key={movies.imdbId} />
-    ))}
-  </div>
+const compareMoviesByDate = (movieA, movieB) => {
+  const dateA = new Date(movieA.releaseDate);
+  const dateB = new Date(movieB.releaseDate);
 
-);
+  return dateA - dateB;
+};
+
+export const MovieList = ({ moviesFromServer }) => {
+  // Упорядочиваем фильмы по дате
+  const orderedMovies = moviesFromServer?.sort(compareMoviesByDate);
+
+  return (
+    <div className="movies">
+      {orderedMovies?.map(movie => (
+        <MovieCard movie={movie} key={movie.imdbId} />
+      ))}
+    </div>
+  );
+};
