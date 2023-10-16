a6f7f681ba7614d536edca46229f934d75c4f6cd
Hryhorii Starykov <hr.starykov@gmail.com>
2023-10-16 10:44:19 +0300

    Fixed

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -5,7 +5,7 @@ export const MovieList = ({ movies }) => (
   <>
     <div className="movies">
       {movies.map(movie => (
-        <MovieCard movies={movies} key={movie.imdbId} />
+        <MovieCard movie={movie} key={movie.imdbId} />
       ))}
     </div>
   </>
