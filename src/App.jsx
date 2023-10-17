1cf258f68b839f2688974cba771e844e181b9b56
Bohdan Antipenko <dr.antipenko2016@gmail.com>
2023-10-17 20:21:24 +0300

    littel fix

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { MovieList } from './components/MovieList';
 export const App = () => (
   <div className="page">
     <div className="page-content">
-      <MovieList movies={movies} />
+      <MovieList movie={movies} />
     </div>
 
     <div className="sidebar" data-cy="Sidebar">
