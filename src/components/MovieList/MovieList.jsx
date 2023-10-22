efaeb52634b305446136f4180ec50665a0466856
Ivan Orinchak <124816409+IvanOrinchak@users.noreply.github.com>
2023-10-22 21:05:44 +0300

    movies

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,5 +1,18 @@
 import './MovieList.scss';
+// import mooviesFilm from '../../api/movies.json';
+import { MovieCard } from '../MovieCard';
 
-export const MovieList = () => (
-  <>Put the list here</>
+export const MovieList = ({ movies }) => (
+  <>
+    {movies.length > 0 ? (
+      <div className="movies">
+        {movies.map(moviess => (
+          <MovieCard
+            movie={moviess}
+            key={moviess.imdbId}
+          />
+        ))}
+      </div>
+    ) : null}
+  </>
 );
