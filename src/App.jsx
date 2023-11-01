17a875f32b12c3f015cfb96c46d0d5c4a55694de
Olena Shapkun <shapkunkh@gmail.com>
2023-11-01 13:51:19 +0200

    ready moviecard

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 /* eslint-disable max-len */
-import { MoviesList } from './components/MovieList';
+import { MovieList } from './components/MovieList';
 import moviesFromServer from './api/movies.json';
 import './App.scss';
 
 export const App = () => (
   <div className="page">
     <div className="page-content">
-      <MoviesList movies={moviesFromServer} />
+      <MovieList movies={moviesFromServer} />
     </div>
 
     <div className="sidebar" data-cy="Sidebar">
