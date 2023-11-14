41c576308cfd119086402898fc2c883119b6d155
Kukharuk <its.up.to.you.dk@gmail.com>
2023-11-14 21:37:49 +0100

    fix

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -2,7 +2,6 @@ import './MovieList.scss';
 import { MovieCard } from '../MovieCard';
 
 export const MovieList = ({ movies }) => (
-
   <div className="movies">
     {movies.map(movie => (
       <MovieCard movie={movie} key={movie.imdbId} />
