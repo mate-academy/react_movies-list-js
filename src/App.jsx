fe1deaf59fbc239d80594e9e98116fdfc3f868b4
Viktor Shaparenko <vkshadyafk@gmail.com>
2023-11-20 15:40:43 +0200

    fix mistake

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@
 
 import './App.scss';
 import { MovieList } from './components/MovieList/MovieList';
-import movies from './api/movies.json';
+import moviesFromServer from './api/movies.json';
 
 export const App = () => (
   <div className="page">
     <div className="page-content">
-      <MovieList movies={movies} />
+      <MovieList movies={moviesFromServer} />
     </div>
     <div className="sidebar" data-cy="Sidebar">
       Sidebar will be here
