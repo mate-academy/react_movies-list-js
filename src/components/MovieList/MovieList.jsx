d5af4fd5373d3e5d8f9459a7f16a1f16e4323267
Fluebubble <Trots.anatoly@gmail.com>
2023-11-20 19:39:43 +0200

    finished

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,5 +1,14 @@
+import { MovieCard } from '../MovieCard';
 import './MovieList.scss';
 
-export const MovieList = () => (
-  <>Put the list here</>
-);
+export const MovieList = (props) => {
+  const { movies } = props;
+
+  return (
+    <div className="movies">
+      {movies.map(
+        movie => <MovieCard movie={movie} key={movie.imdbId} />,
+      )}
+    </div>
+  );
+};
