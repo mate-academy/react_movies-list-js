e50d0f759f45999db48dd06b613aaf094c73e5da
Egor <yehorheneralov@gmail.com>
2023-11-20 17:53:27 +0200

    first try1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 
 import './App.scss';
-// import moviesFromServer from './api/movies.json';
+import moviesFromServer from './api/movies.json';
 import { MovieList } from './components/MovieList';
 
 export const App = () => (
   <div className="page">
     <div className="page-content">
-      <MovieList />
+      <MovieList movies={moviesFromServer} />
     </div>
 
     <div className="sidebar" data-cy="Sidebar">
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,11 +1,11 @@
 import './MovieList.scss';
 import { MovieCard } from '../MovieCard/MovieCard';
-import moviesFromServer from '../../api/movies.json';
+// import moviesFromServer from '../../api/movies.json';
 
-export const MovieList = () => (
-  moviesFromServer.length !== 0 && (
+export const MovieList = ({ movies }) => (
+  movies.length !== 0 && (
     <div className="movies">
-      {moviesFromServer.map(movie => (
+      {movies.map(movie => (
         <MovieCard movie={movie} key={movie.imdbId} />
       ))}
     </div>
