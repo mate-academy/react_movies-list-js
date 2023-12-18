0d76ce0bb0ea83b31a31918fcd4a1e9b93220e13
Vladyslav Zakharov <vladyslavzakharovdk@gmail.com>
2023-12-18 20:12:08 +0200

    add for test

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,5 +1,10 @@
 import './MovieList.scss';
+import { MovieCard } from '../MovieCard';
 
-export const MovieList = () => (
-  <>Put the list here</>
+export const MovieList = ({ movies }) => (
+  <div className="card" data-cy="Movie">
+    {movies.map(movie => (
+      <MovieCard movie={movie} key={movie.imdbId} />
+    ))}
+  </div>
 );
