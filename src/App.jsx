9f3178ead5cadd4169e690f021b6831949fe192c
TVChulkova <6.02tatyana@gmail.com>
2023-12-23 14:46:45 +0200

    solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,43 @@
 /* eslint-disable max-len */
+import { useState } from 'react';
 
 import './App.scss';
 import { MovieList } from './components/MovieList';
 import moviesFromServer from './api/movies.json';
 
-export const App = () => (
-  <div className="page">
-    <div className="page-content">
-      <MovieList movies={moviesFromServer} />
-    </div>
+function getPreparedMovies(movies, { query }) {
+  const queryStr = query.trim().toLowerCase();
+  let prepearedMovies = [...movies];
 
-    <div className="sidebar" data-cy="Sidebar">
-      Sidebar will be here
+  if (query) {
+    prepearedMovies = prepearedMovies.filter(
+      movie => movie.title.toLowerCase().includes(queryStr)
+        || movie.description.toLowerCase().includes(queryStr),
+    );
+  }
+
+  return prepearedMovies;
+}
+
+export const App = () => {
+  const [query, setQuery] = useState('');
+  const visibleMovies = getPreparedMovies(moviesFromServer, { query });
+
+  return (
+    <div className="page">
+      <input
+        type="text"
+        className="search-field"
+        onChange={event => setQuery(event.target.value)}
+      />
+      <br />
+      <div className="page-content">
+        <MovieList movies={visibleMovies} />
+      </div>
+
+      <div className="sidebar" data-cy="Sidebar">
+        Sidebar will be here
+      </div>
     </div>
-  </div>
-);
+  );
+};
