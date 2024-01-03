6441d3c4e360dc4ed1219b28d99491a85b027e8d
Andrew Rich <andy.rich3a@gmail.com>
2024-01-03 14:26:05 +0200

    add task fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,53 +3,6 @@
 import './App.scss';
 import { MovieList } from './components/MovieList';
 import moviesFromServer from './api/movies.json';
-// import { MovieCard } from './components/MovieCard';
-
-// const MovieCard = ({ movie }) => (
-//   <div
-//     className="card"
-//     data-cy="Movie"
-//   >
-//     <div className="card-image">
-//       <figure className="image is-4by3">
-//         <img
-//           data-cy="MovieImage"
-//           src={movie.imgUrl}
-//           alt="Film logo"
-//         />
-//       </figure>
-//     </div>
-
-//     <div className="card-content">
-//       <div className="media">
-//         <div className="media-left">
-//           <figure className="image is-48x48">
-//             <img src="images/imdb-logo.jpeg" alt="imdb" />
-//           </figure>
-//         </div>
-
-//         <div className="media-content">
-//           <p className="title is-8" data-cy="MovieTitle">
-//             {movie.title}
-//           </p>
-//         </div>
-//       </div>
-
-//       <div className="content">
-//         <p data-cy="MovieDescription">
-//           {movie.description}
-//         </p>
-
-//         <a
-//           href={movie.imdbUrl}
-//           data-cy="MovieLink"
-//         >
-//           IMDB
-//         </a>
-//       </div>
-//     </div>
-//   </div>
-// );
 
 export const App = () => (
   <div className="page">
