99fc0b05f271114cebc55135090e080016457b52
Maryna Shmyhelska <shmigelska.m.y@gmail.com>
2024-01-15 15:58:13 +0200

    add task solution

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,5 +1,13 @@
+import { MovieCard } from '../MovieCard';
 import './MovieList.scss';
 
-export const MovieList = () => (
-  <>Put the list here</>
+export const MovieList = ({ movies }) => (
+  <div className="movies">
+    {movies.map(film => (
+      <MovieCard
+        key={film.imdbId}
+        movie={film}
+      />
+    ))}
+  </div>
 );
