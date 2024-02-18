7f61d9be95523126642c80a79d2bb38b64a0b8bc
Oleh Khrystofor <hristoforolegivanovich@gmail.com>
2024-02-18 17:25:50 +0200

    add first after changing settings VS Code

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,18 @@ import './App.scss';
 import moviesFromServer from './api/movies.json';
 import { MovieList } from './components/MovieList';
 
-export const App = () => (
-  <div className="page">
-    <div className="page-content">
-      <MovieList movies={moviesFromServer} />
-    </div>
+export const App = () => {
+  console.log('a');
 
-    <div className="sidebar" data-cy="Sidebar">
-      Sidebar will be here
+  return (
+    <div className="page">
+      <div className="page-content">
+        <MovieList movies={moviesFromServer} />
+      </div>
+
+      <div className="sidebar" data-cy="Sidebar">
+        Sidebar will be here
+      </div>
     </div>
-  </div>
-);
+  );
+};
