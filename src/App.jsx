6557400e4cf84ce1825d232ad8cae7ce02f5af80
Oleh Khrystofor <hristoforolegivanovich@gmail.com>
2024-02-18 17:28:53 +0200

    second add

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 /* eslint-disable max-len */
 
+import { useState } from 'react';
 import './App.scss';
 import moviesFromServer from './api/movies.json';
 import { MovieList } from './components/MovieList';
 
 export const App = () => {
-  console.log('a');
+  const [moviesToRend] = useState(moviesFromServer);
+  const moviesToRendVar = moviesToRend;
 
   return (
     <div className="page">
       <div className="page-content">
-        <MovieList movies={moviesFromServer} />
+        <MovieList movies={moviesToRendVar} />
       </div>
 
       <div className="sidebar" data-cy="Sidebar">
