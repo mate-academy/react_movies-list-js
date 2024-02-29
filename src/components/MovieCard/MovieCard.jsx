b81eb84d491d6ba7f23a8d231612043201bd80f9
Denys209818 <deniskravcuk63@gmail.com>
2024-02-29 21:36:02 +0200

    Load realization

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,3 +1,44 @@
 import './MovieCard.scss';
 
-export const MovieCard = () => <>Put the card here</>;
+export const MovieCard = ({card}) => (
+  <div className="card" data-cy="Movie">
+    <div className="card-image">
+      <figure className="image is-4by3">
+        <img
+          data-cy="MovieImage"
+          src={card.imgUrl}
+          alt="Film logo"
+        />
+      </figure>
+    </div>
+
+    <div className="card-content">
+      <div className="media">
+        <div className="media-left">
+          <figure className="image is-48x48">
+            <img src={card.imdbUrl} alt="imdb" />
+          </figure>
+        </div>
+
+        <div className="media-content">
+          <p className="title is-8" data-cy="MovieTitle">
+            {card.title}
+          </p>
+        </div>
+      </div>
+
+    <div className="content">
+      <p data-cy="MovieDescription">
+        {card.description}
+      </p>
+
+      <a
+        href={card.imdbUrl}
+        data-cy="MovieLink"
+      >
+        IMDB
+      </a>
+    </div>
+    </div>
+  </div>
+);
