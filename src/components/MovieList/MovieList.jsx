f11408d456f543ea349d84d2af40c9ae2bfbadff
Vitalii <vitalikspilman@gmail.com>
2024-03-27 15:54:49 +0200

    Remove check

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -3,11 +3,8 @@ import './MovieList.scss';
 
 export const MovieList = ({ moviesFromServer }) => (
   <div className="movies">
-    {moviesFromServer.length
-      ? moviesFromServer.map(movie => (
-          <MovieCard movie={movie} key={movie.imdbId} />
-          // eslint-disable-next-line indent
-        ))
-      : ''}
+    {moviesFromServer.map(movie => (
+      <MovieCard movie={movie} key={movie.imdbId} />
+    ))}
   </div>
 );
