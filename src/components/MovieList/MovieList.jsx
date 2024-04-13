8222da420ad4bc1b1c6ad038188b396dff680224
beliyandr <beliyandr@gmail.com>
2024-04-13 23:32:10 +0300

    first

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,3 +1,14 @@
 import './MovieList.scss';
+import { MovieCard } from '../MovieCard';
 
-export const MovieList = () => <>Put the list here</>;
+export const MovieList = ({ movies }) => {
+  return (
+    <div className="movies">
+      {movies.map((movie) => {
+        return (
+          <MovieCard movie={movie} key={movie.imdbId} />
+        )
+      })}
+    </div>
+  )
+}
