f41d1209f4f7060833f75e874eabfb688588f696
Prytula Olexandr <liga5872@gmail.com>
2024-04-16 16:40:56 +0300

    add task

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,9 @@ export const App = () => (
   <div className="page">
     <div className="page-content">
       <MovieList movies={movies} />
-      <div className="sidebar" data-cy="Sidebar">
-        Sidebar will be here
-      </div>
+    </div>
+    <div className="sidebar" data-cy="Sidebar">
+      Sidebar will be here
     </div>
   </div>
 );
