3264efbda862f79ef94c18b724a548c60d02f433
Oleksii Shpak <willwildred@gmail.com>
2024-04-17 15:04:00 +0200

    implemented solution

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import moviesFromServer from './api/movies.json';
 export const App = () => (
   <div className="page">
     <div className="page-content">
-      <MovieList moviesFromServer={moviesFromServer} />
+      <MovieList movies={moviesFromServer} />
     </div>
 
     <div className="sidebar" data-cy="Sidebar">
