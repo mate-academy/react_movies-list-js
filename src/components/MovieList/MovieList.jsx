42690ff6b74c4f81e51318757e7c5a0e94427b9f
Greshch <greshchook@gmail.com>
2024-04-21 20:43:32 +0300

    2

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,38 +1,40 @@
 import './MovieList.scss';
 
+export const MovieCard = ({ movie }) => (
+  <div className="card" data-cy="Movie">
+    <div className="card-image">
+      <figure className="image is-4by3">
+        <img data-cy="MovieImage" src={movie.imgUrl} alt="Film logo" />
+      </figure>
+    </div>
+
+    <div className="card-content">
+      <div className="media">
+        <div className="media-left">
+          <figure className="image is-48x48">
+            <img src="images/imdb-logo.jpeg" alt="imdb" />
+          </figure>
+        </div>
+
+        <div className="media-content">
+          <p className="title is-8" data-cy="MovieTitle">
+            {movie.title}
+          </p>
+        </div>
+      </div>
+
+      <div className="content">
+        <p data-cy="MovieDescription">{movie.description}</p>
+        <a href={movie.imdbUrl}>IMDB</a>
+      </div>
+    </div>
+  </div>
+);
+
 export const MoveList = ({ movies }) => (
   <div className="movies">
-    {movies.map(movie => {
-      return (
-        <div key={movie.imdbId} className="card" data-cy="Movie">
-          <div className="card-image">
-            <figure className="image is-4by3">
-              <img data-cy="MovieImage" src={movie.imgUrl} alt="Film logo" />
-            </figure>
-          </div>
-
-          <div className="card-content">
-            <div className="media">
-              <div className="media-left">
-                <figure className="image is-48x48">
-                  <img src="images/imdb-logo.jpeg" alt="imdb" />
-                </figure>
-              </div>
-
-              <div className="media-content">
-                <p className="title is-8" data-cy="MovieTitle">
-                  {movie.title}
-                </p>
-              </div>
-            </div>
-
-            <div className="content">
-              <p data-cy="MovieDescription">{movie.description}</p>
-              <a href={movie.imdbUrl}>IMDB</a>
-            </div>
-          </div>
-        </div>
-      );
-    })}
+    {movies.map(movie => (
+      <MovieCard key={movie.imdbId} movie={movie} />
+    ))}
   </div>
 );
