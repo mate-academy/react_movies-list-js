38d2c9bf561db08fbb8f85b6b060f7b5af17e556
Dmytro Kupriiev <d.kupriev@gmail.com>
2024-04-22 22:43:00 +0300

    fix key in movieList children

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -4,7 +4,7 @@ import './MovieList.scss';
 export const MovieList = ({ movies }) => (
   <div className="movies">
     {movies.map(movie => (
-      <MovieCard movie={movie} />
+      <MovieCard movie={movie} key={movie.imdbId} />
     ))}
   </div>
 );
