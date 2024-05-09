2642bffb98384fcb5ea68de6534b2e290135e3e1
Nataliia <gzodchie@gmail.com>
2024-05-09 13:11:47 +0300

    fixed MovieList delete redundant div

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -3,10 +3,8 @@ import './MovieList.scss';
 
 export const MovieList = ({ movies }) => (
   <div className="movies">
-    <div className="card" data-cy="Movie">
-      {movies.map(movie => (
-        <MovieCard movie={movie} key={movie.imdbId} />
-      ))}
-    </div>
+    {movies.map(movie => (
+      <MovieCard movie={movie} key={movie.imdbId} />
+    ))}
   </div>
 );
