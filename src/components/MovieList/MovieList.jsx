e45d0c6eb7f65ebe00246cbf9f001aebfcbd688c
Oleksandr Goryachun <aleks.goryachun@gmail.com>
2024-05-22 16:17:56 +0300

    add task solution

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,3 +1,12 @@
+/* eslint-disable max-len */
 import './MovieList.scss';
+import React from 'react';
+import { MovieCard } from '../MovieCard';
 
-export const MovieList = () => <>Put the list here</>;
+export const MovieList = ({ movies }) => (
+  <div className="movies">
+    {movies.map(movie => (
+      <MovieCard movie={movie} key={movie.imdbId} />
+    ))}
+  </div>
+);
