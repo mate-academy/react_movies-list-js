76da767f5718e1bd19970794283e65f4f616c6b1
Oleksii Karpov <alneon1@gmail.com>
2024-12-12 16:59:44 +0200

    min changes added v.2

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -4,7 +4,7 @@ import './MovieList.scss';
 export const MovieList = ({ movies }) => (
   <div className="movies">
     {movies.map(movie => (
-      <MovieCard card={movie} key={movie.imdbId} />
+      <MovieCard movie={movie} key={movie.imdbId} />
     ))}
   </div>
 );
