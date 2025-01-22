4a9c364d53aa5ce34387446034b5a839bb3324c5
Artem Zazeka <zazekaartemka01@gmail.com>
2025-01-22 17:02:15 +0400

    add solution

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,3 +1,33 @@
+import React from 'react';
 import './MovieCard.scss';
 
-export const MovieCard = () => <>Put the card here</>;
+export const MovieCard = ({ movie }) => (
+  <div className="card" data-cy="Movie">
+    <div className="card-image">
+      <figure className="image is-4by3">
+        <img
+          data-cy="MovieImage"
+          src={movie.imgUrl}
+          alt={`${movie.title} poster`}
+        />
+      </figure>
+    </div>
+
+    <div className="card-content">
+      <div className="media">
+        <div className="media-content">
+          <p className="title is-4" data-cy="MovieTitle">{movie.title}</p>
+          <p className="subtitle is-6" data-cy="MovieYear">{movie.year}</p>
+        </div>
+      </div>
+
+      <div className="content">
+      <p data-cy="MovieDescription">{movie.description}</p>
+
+        <a href={`https://www.imdb.com/title/${movie.imdbId}`} target="_blank" rel="noopener noreferrer" data-cy="MovieLink">
+          View on IMDb
+        </a>
+      </div>
+    </div>
+  </div>
+);
