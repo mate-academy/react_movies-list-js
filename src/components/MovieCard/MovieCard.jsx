2bfd1f85b5be4efa5dc7fc1f87f76b57be291592
Marius <mariuss342@gmail.com>
2025-01-29 18:50:05 +0200

    add task solution

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,3 +1,41 @@
 import './MovieCard.scss';
 
-export const MovieCard = () => <>Put the card here</>;
+export const MovieCard = ({ movie }) => (
+  <div className="card" data-cy="Movie">
+    {/* Movie Poster */}
+    <div className="movie-poster">
+      <img
+        data-cy="MovieImage"
+        src={
+          movie.imageUrl ||
+          'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg'
+        } // Add a fallback image
+        alt={`${movie.title} poster`}
+      />
+    </div>
+
+    <div className="card-content">
+      <div className="media">
+        <div className="media-left">
+          <figure className="image is-48x48">
+            <img src="images/imdb-logo.jpeg" alt="imdb" />
+          </figure>
+        </div>
+
+        <div className="media-content">
+          <p className="title is-8" data-cy="MovieTitle">
+            {movie.title}
+          </p>
+        </div>
+      </div>
+
+      <div className="content">
+        <p data-cy="MovieDescription">{movie.description}</p>
+
+        <a href={movie.imdbUrl} data-cy="MovieLink">
+          IMDB
+        </a>
+      </div>
+    </div>
+  </div>
+);
