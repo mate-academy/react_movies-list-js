a5ad6563857fa293b917166d6c7b73ebe09c8512
tiblazy <hauntedapply@gmail.com>
2025-02-09 00:27:22 -0300

    split components

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,3 +1,5 @@
+import { MovieCard } from '../MovieCard/MovieCard';
 import './MovieList.scss';
 
-export const MovieList = () => <>Put the list here</>;
+export const MovieList = ({ movies }) =>
+  movies.map(movie => <MovieCard movie={movie} />);
