36f712559486acb71de8138c62e2fd502d079359
Iryna <irakogut23190@gmail.com>
2025-02-13 11:40:03 +0200

    soltion

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,3 +1,9 @@
+import { MovieCard } from '../MovieCard/MovieCard';
 import './MovieList.scss';
 
-export const MovieList = () => <>Put the list here</>;
+export const MovieList = ({ movies }) =>
+  movies.map(movie => (
+    <div className="movies">
+      <MovieCard movie={movie} key={movie.imdbId} />
+    </div>
+  ));
