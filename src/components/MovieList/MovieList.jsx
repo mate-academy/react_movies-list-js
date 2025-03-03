62d33cc47ccea2b99f118ec95f92a0ad7a9b6ea3
SofiiaPelenska <soniapelenskaoe2002@gmail.com>
2025-03-03 17:56:34 +0200

    fix imports path

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,5 +1,5 @@
 import './MovieList.scss';
-import { MovieCard } from '../MovieCard/MovieCard';
+import { MovieCard } from '../MovieCard';
 
 export const MovieList = ({ movies }) => (
   <div className="movies">
