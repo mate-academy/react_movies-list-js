14b8c83d86cb23562d4c6ce64345a8fc49e00923
Nadiia Bulmak <nadgaval@gmail.com>
2025-03-06 18:00:14 +0200

    add render solution

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,3 +1,17 @@
+import { MovieCard } from '../MovieCard/MovieCard';
+// import moviesFromServer from '../../api/movies.json';
 import './MovieList.scss';
 
-export const MovieList = () => <>Put the list here</>;
+export const MovieList = ({ movies }) => {
+  if (movies.length === 0) {
+    return null;
+  }
+
+  return (
+    <div className="movies">
+      {movies.map(movie => (
+        <MovieCard movie={movie} key={movie.imdbId} />
+      ))}
+    </div>
+  );
+};
