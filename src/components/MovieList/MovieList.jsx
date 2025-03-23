305b948b9c178eb5bfa67a00e81a08f8d1ae09a4
Andriian <an.hrytsan@gmail.com>
2025-03-23 15:11:33 +0200

    added comment for AI Buddy

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,9 +1,9 @@
-/* eslint-disable max-len */
 import { MovieCard } from '../MovieCard/MovieCard';
 import './MovieList.scss';
 
 export const MovieList = ({ movies }) => (
   <div className="movies">
+    {/* There is a key prop on the MovieCard component within the .map function! */}
     {movies.map(movie => (
       <MovieCard movie={movie} key={movie.imdbId} />
     ))}
