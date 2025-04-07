f41236b97d62a7ecaec1b3f8b92eb4a18e962425
Tim <drdakkadakka@gmail.com>
2025-04-07 18:46:05 +0200

    1st

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,3 +1,17 @@
 import './MovieList.scss';
+import { MovieCard } from '../MovieCard';
+import movies from '../../api/movies.json';
 
-export const MovieList = () => <>Put the list here</>;
+export const MovieList = () => (
+  <div className="movies">
+    {movies.map(movie => (
+      <MovieCard
+        key={movie.imdbId}
+        title={movie.title}
+        description={movie.description}
+        imgUrl={movie.imgUrl}
+        imdbUrl={movie.imdbUrl}
+      />
+    ))}
+  </div>
+);
