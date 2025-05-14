a5569e41226b5753d2b97967a22ec4a5daaeca22
torry455 <kucerv8@gmail.com>
2025-05-14 17:17:46 +0300

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@
 
 import './App.scss';
 import { MovieList } from './components/MovieList/MovieList';
+import movies from './api/movies.json';
 
 export const App = () => (
   <div className="page">
     <div className="page-content">
-      <MovieList />
+      <MovieList movies={movies} />
 
       <div className="sidebar" data-cy="Sidebar">
         Sidebar will be here
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,8 +1,7 @@
 import { MovieCard } from '../MovieCard/MovieCard';
-import movies from '../../api/movies.json';
 import './MovieList.scss';
 
-export const MovieList = () => (
+export const MovieList = ({ movies }) => (
   <div className="movies">
     {movies.map(movie => (
       <MovieCard movie={movie} key={movie.imdbId} />
