99612cb30c0c902b18c3c94f206e60d5b8002403
Nikolay <diedaemon@gmail.com>
2025-05-26 14:02:26 +0200

    task complete

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -5,7 +5,7 @@ import { MovieCard } from '../MovieCard/MovieCard';
 export const MovieList = ({ movies }) => (
   <div className="movies">
     {movies.map(movie => (
-      <MovieCard mov={movie} key={movie.imdbId} />
+      <MovieCard movie={movie} key={movie.imdbId} />
     ))}
   </div>
 );
