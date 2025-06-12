0b2dacf2043ce0b417729a14058156d9b7a99cf0
Aket0r <zerotwozefrankxx@gmail.com>
2025-06-12 23:16:22 +0200

    solution

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,3 +1,21 @@
+/* eslint-disable prettier/prettier */
+import { MovieCard } from '../MovieCard/MovieCard';
+import moviesJSON from '../../api/movies.json';
 import './MovieList.scss';
 
-export const MovieList = () => <>Put the list here</>;
+export const MovieList = ({ movies = moviesJSON }) => {
+  return (
+    <div className="movies">
+        {(movies.length > 0) ? (
+          movies.map((movie) => (
+            <MovieCard
+              key={movie.imdbId}
+              movie={movie}
+            />
+          ))
+        ) : (
+          null
+        )}
+    </div>
+  );
+};
