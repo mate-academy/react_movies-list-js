b39eb2361c3adea4d08edf14d5b80cb330790176
Anatoli Yaschenko <yachenko.anatoliy37@gmail.com>
2025-06-27 14:41:49 +0300

    повідомлення

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,3 +1,20 @@
+// MovieCard.jsx
 import './MovieCard.scss';
 
-export const MovieCard = () => <>Put the card here</>;
+export const MovieCard = ({ movie }) => {
+  return (
+    <div className="movie-card">
+      <h2 data-cy="MovieTitle">{movie.title}</h2>
+      <p data-cy="MovieDescription">{movie.description}</p>
+      <a
+        data-cy="MovieLink"
+        href={movie.imdbUrl}
+        target="_blank"
+        rel="noreferrer"
+      >
+        IMDb
+      </a>
+      <img data-cy="MovieImage" src={movie.imgUrl} alt={movie.title} />
+    </div>
+  );
+};
