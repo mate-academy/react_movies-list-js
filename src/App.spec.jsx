8bea0652727a056fe882ce6bff78bbe1c23f6abe
Nádia <nadia.mfaria@hotmail.com>
2025-07-14 23:15:50 -0300

    Solution

--- src/App.spec.jsx
+++ src/App.spec.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { mount } from '@cypress/react18';
 import { App } from './App';
 
 describe('App', () => {
   beforeEach(() => {
-    mount(<App />);
+    cy.mount(<App />);
   });
 
   it('should render all the movies', () => {
