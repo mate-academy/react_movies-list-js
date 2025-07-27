aa7954868e5841c0484c76a4a900724e7b3334bf
michelam-pseudo <124099775+michelam-pseudo@users.noreply.github.com>
2025-07-27 01:06:11 -0300

    Refinamento para correção de URL incorreta - URL da logo IMDB

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,7 @@ import { MovieList } from './components/MovieList/MovieList';
 export const App = () => (
   <div className="page">
     <div className="page-content">
-      <MovieList 
-        movies={movies}
-      />
+      <MovieList movies={movies} />
     </div>
 
     <div className="sidebar" data-cy="Sidebar">
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,18 +1,12 @@
 import './MovieList.scss';
-import {MovieCard} from '../MovieCard/MovieCard.jsx';
+import { MovieCard } from '../MovieCard/MovieCard';
 
-export const MovieList = ({movies}) => {
-    return(
-        <>
-            <div className="movies">
-                {
-                    movies.map(movie => {
-                        return (
-                            <MovieCard movie={movie} />
-                        )
-                    })
-                }
-            </div>
-        </>
-    )
-}
+export const MovieList = ({ movies }) => {
+  return (
+    <div className="movies">
+      {movies.map(movie => {
+        return <MovieCard movie={movie} />;
+      })}
+    </div>
+  );
+};
