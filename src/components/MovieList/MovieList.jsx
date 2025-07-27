68aae2dba920a7cead3895dc33d66b10f201b071
michelam-pseudo <124099775+michelam-pseudo@users.noreply.github.com>
2025-07-27 00:58:03 -0300

    Concretização da decomposição orientada a componentes sob os requisitos especificados

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,3 +1,18 @@
 import './MovieList.scss';
+import {MovieCard} from '../MovieCard/MovieCard.jsx';
 
-export const MovieList = () => <>Put the list here</>;
+export const MovieList = ({movies}) => {
+    return(
+        <>
+            <div className="movies">
+                {
+                    movies.map(movie => {
+                        return (
+                            <MovieCard movie={movie} />
+                        )
+                    })
+                }
+            </div>
+        </>
+    )
+}
