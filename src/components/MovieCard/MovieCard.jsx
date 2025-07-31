1806af625b1b1d6d6389ee9f7686d8977688244f
Вадим <vadym@MacBook-Pro-Vadim.local>
2025-07-31 13:16:18 +0300

    First commit

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,3 +1,19 @@
 import './MovieCard.scss';
 
-export const MovieCard = () => <>Put the card here</>;
+export const MovieCard = ({ movie }) => {
+  return (
+    <div className="card" data-cy="Movie">
+      <img data-cy="MovieImage" src={movie.imgUrl} alt={movie.title} />
+      <h2 data-cy="MovieTitle">{movie.title}</h2>
+      <p data-cy="MovieDescription">{movie.description}</p>
+      <a
+        data-cy="MovieLink"
+        href={movie.imdbUrl}
+        target="_blank"
+        rel="noreferrer"
+      >
+        Go to IMDb
+      </a>
+    </div>
+  );
+};
