2ad1275b7c67b6fd8c26bdff5c5a7bced92037fa
Emaan Hookey <em.grey28@gmail.com>
2025-08-02 18:37:41 -0230

    followed instructions

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,3 +1,16 @@
+import React from 'react';
 import './MovieCard.scss';
 
-export const MovieCard = () => <>Put the card here</>;
+export const MovieCard = ({ movie }) => (
+  <div data-cy="card" className="card">
+    <h2 data-cy="title" className="card__title">
+      {movie.title}
+    </h2>
+    <img
+      data-cy="img"
+      className="card__img"
+      src={movie.img}
+      alt={`${movie.title} poster`}
+    />
+  </div>
+);
