b5970a10d2451cf315536fec0889578abf09b75c
serhiy23471 <sergsy.pyatachenko@gmail.com>
2025-08-19 12:46:13 +0300

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import './App.scss';
-import moviesFromServer from './api/movies.json';
-import { MovieList } from './components/MovieList';
+import movies from './api/movies.json';
+import { MovieList } from './components/MovieList/MovieList';
 
 export const App = () => (
   <div className="page">
     <div className="page-content">
-      <MovieList movies={moviesFromServer} />
+      <MovieList movies={movies} />
     </div>
 
     <div className="sidebar" data-cy="Sidebar">
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -2,10 +2,12 @@ import './MovieList.scss';
 
 import { MovieCard } from '../MovieCard/MovieCard';
 
-export const MovieList = ({ movies }) => (
-  <div className="movies">
-    {movies.map(movie => (
-      <MovieCard key={movie.imdbId} movie={movie} />
-    ))}
-  </div>
-);
+export const MovieList = ({ movies = [] }) => {
+  return (
+    <div className="movies">
+      {movies.map(movie => (
+        <MovieCard key={movie.imdbId} movie={movie} />
+      ))}
+    </div>
+  );
+};
