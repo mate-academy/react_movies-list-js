24b113b428e17b0ed2a3650143a6ff9a4d6b67e6
kirky359 <lol783176@gmail.com>
2025-09-05 19:48:17 +0300

    completed

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,3 +1,12 @@
 import './MovieList.scss';
+import { MovieCard } from '../MovieCard';
 
-export const MovieList = () => <>Put the list here</>;
+/* eslint-disable max-len */
+export const MovieList = ({ movies }) => (
+  <div className="movies">
+    {movies.map(movie => (
+      <MovieCard movie={movie} key={movie.imdbId} />
+    ))}
+    ;
+  </div>
+);
