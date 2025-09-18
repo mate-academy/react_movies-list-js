53cb247cbbdd9e86425e40fa55419bf6912b368d
TetiankaSh <shh.tetiana@gmail.com>
2025-09-18 18:47:51 +0300

    commit to see the issues

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,3 +1,19 @@
 import './MovieList.scss';
+import movies from '../../api/movies.json';
+import { MovieCard } from '../MovieCard/MovieCard';
 
-export const MovieList = () => <>Put the list here</>;
+export const MovieList = () => {
+  return (
+    <div className="movie-list">
+      {movies.map(movie => (
+        <MovieCard
+          key={movie.imdbId}
+          title={movie.title}
+          description={movie.description}
+          imgUrl={movie.imgUrl}
+          imdbUrl={movie.imdbUrl}
+        />
+      ))}
+    </div>
+  );
+};
