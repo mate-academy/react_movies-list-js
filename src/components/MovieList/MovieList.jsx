599b7a956e04ea696784ef318b4f8c2a7a4813f1
Mariana Melo <marimajorana@hotmail.com>
2025-09-25 13:46:33 -0300

    Solution

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,3 +1,14 @@
 import './MovieList.scss';
+import { MovieCard } from '../MovieCard/MovieCard';
 
-export const MovieList = () => <>Put the list here</>;
+export const MovieList = ({ movies }) => {
+  const sorted = [...movies].sort((a, b) => a.title.localeCompare(b.title));
+
+  return (
+    <div className="movies">
+      {sorted.map(movie => (
+        <MovieCard key={movie.imdbId} movie={movie} />
+      ))}
+    </div>
+  );
+};
