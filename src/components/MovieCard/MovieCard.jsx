e3089af4c507a08a7110dd5b8dbfd54078f9364c
milton <mitnunes.pro.com>
2025-09-29 15:33:26 -0300

    add1

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,3 +1,53 @@
 import './MovieCard.scss';
 
-export const MovieCard = () => <>Put the card here</>;
+//export const MovieCard = () => <>Put the card here</>;
+import React from 'react';
+import './MovieCard.scss';
+
+export const MovieCard = ({
+  imageSrc,
+  title,
+  description,
+  imdbLink,
+}) => (
+  <div className="card" data-cy="Movie">
+    <div className="card-image">
+      <figure className="image is-4by3">
+        <img
+          data-cy="MovieImage"
+          src={imageSrc}
+          alt="Film logo"
+        />
+      </figure>
+    </div>
+
+    <div className="card-content">
+      <div className="media">
+        <div className="media-left">
+          <figure className="image is-48x48">
+            <img src="images/imdb-logo.jpeg" alt="imdb" />
+          </figure>
+        </div>
+
+        <div className="media-content">
+          <p className="title is-8" data-cy="MovieTitle">
+            {title}
+          </p>
+        </div>
+      </div>
+
+      <div className="content">
+        <p data-cy="MovieDescription">
+          {description}
+        </p>
+
+        <a
+          href={imdbLink}
+          data-cy="MovieLink"
+        >
+          IMDB
+        </a>
+      </div>
+    </div>
+  </div>
+);
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,3 +1,27 @@
 import './MovieList.scss';
 
-export const MovieList = () => <>Put the list here</>;
+//export const MovieList = () => <>Put the list here</>;
+import './MovieCard.scss';
+import React from 'react';
+import { MovieCard } from '../MovieCard/MovieCard';
+
+const moviesFromServer = [
+  { id: 1, title: 'Inception', year: 2010 },
+  { id: 2, title: 'Interstellar', year: 2014 },
+  { id: 3, title: 'The Dark Knight', year: 2008 },
+];
+
+export const MovieList = ({ movies }) => (
+  <div className="movies">
+    {movies.map(movie => (
+      <MovieCard
+        key={movie.imdbId}
+        imageSrc={movie.imageSrc}
+        title={movie.title}
+        description={movie.description}
+        imdbLink={movie.imdbLink}
+      />
+    ))}
+  </div>
+);
+export default MovieList;
