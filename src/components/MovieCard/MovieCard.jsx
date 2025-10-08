5a8de0c5b967c6f248621ef60501f8d956b7e5e5
Vasya <rusnakvasa3@gmail.com>
2025-10-09 01:22:30 +0300

    a

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,14 +1,10 @@
 import './MovieCard.scss';
 
-export const MovieCard = ({movie}) => <>
+export const MovieCard = ({ movie }) => (
   <div className="card" data-cy="Movie">
     <div className="card-image">
       <figure className="image is-4by3">
-        <img
-          data-cy="MovieImage"
-          src={movie.imgUrl}
-          alt="Film logo"
-        />
+        <img data-cy="MovieImage" src={movie.imgUrl} alt="Film logo" />
       </figure>
     </div>
 
@@ -28,14 +24,11 @@ export const MovieCard = ({movie}) => <>
       </div>
 
       <div className="content">
-        <p data-cy="MovieDescription">
-          {movie.description}
-        </p>
-        <a href={movie.imdbUrl}
-          data-cy="MovieLink">
+        <p data-cy="MovieDescription">{movie.description}</p>
+        <a href={movie.imdbUrl} data-cy="MovieLink">
           IMDB
         </a>
       </div>
     </div>
   </div>
-</>;
+);
