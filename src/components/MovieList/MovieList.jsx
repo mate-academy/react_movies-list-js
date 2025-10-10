3b5550354f1d237a3450a754d1de6e8823932821
TaniaFedortsi <tania.fdrts@gmail.com>
2025-10-10 11:26:19 +0300

    fixed html

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -2,7 +2,7 @@ import './MovieList.scss';
 import { MovieCard } from '../MovieCard';
 
 export const MovieList = ({ movies }) => (
-  <div className="movies">
+  <div className="movies" data-cy="moviesList">
     {movies.map(movie => (
       <MovieCard key={movie.imdbId} movie={movie} />
     ))}
