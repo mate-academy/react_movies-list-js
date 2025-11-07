ac27043116d2bcf7bb251f0686315254b5435949
Iryna Knyzh <iryna.knyzh@gmail.com>
2025-11-07 13:12:00 +0200

    task solution

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,14 +1,10 @@
 import './MovieCard.scss';
 
-export const MovieCard = ({movie}) => <>
-  <div className="card" data-cy="Movie" key={movie.imdbId}>
+export const MovieCard = ({ movie }) => (
+  <div className="card" data-cy="Movie">
     <div className="card-image">
       <figure className="image is-4by3">
-        <img
-          data-cy="MovieImage"
-          src={movie.imgUrl}
-          alt="Film logo"
-        />
+        <img data-cy="MovieImage" src={movie.imgUrl} alt="Film logo" />
       </figure>
     </div>
 
@@ -28,17 +24,12 @@ export const MovieCard = ({movie}) => <>
       </div>
 
       <div className="content">
-        <p data-cy="MovieDescription">
-          {movie.description}
-        </p>
+        <p data-cy="MovieDescription">{movie.description}</p>
 
-        <a
-          href={movie.imdbUrl}
-          data-cy="MovieLink"
-        >
+        <a href={movie.imdbUrl} data-cy="MovieLink">
           IMDB
         </a>
       </div>
     </div>
   </div>
-</>;
+);
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,10 +1,10 @@
 import { MovieCard } from '../MovieCard';
 import './MovieList.scss';
 
-export const MovieList = ({ movies }) => <>
+export const MovieList = ({ movies }) => (
   <div className="movies">
     {movies.map(movie => (
-      <MovieCard movie={movie} />
+      <MovieCard movie={movie}  key={movie.imdbId} />
     ))}
   </div>
-</>;
+);
