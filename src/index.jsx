6b32744e86bb6d3b77bbdc1a608e3e4a48a0fe16
Josias <josiasonesokataborda22@gmail.com>
2025-11-19 14:12:30 -0300

    adicao da lista de filmes do site

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,8 @@
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 import 'bulma/css/bulma.css';
 
 import { App } from './App';
+import './App.scss';
 
 createRoot(document.getElementById('root')).render(<App />);
