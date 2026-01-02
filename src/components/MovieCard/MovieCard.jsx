c14c1e3b4731166a436c3d14a39aff328dfdfcab
Adelina Yovdii <adelincaiovdii@gmail.com>
2026-01-02 12:21:20 +0200

    solution

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -4,7 +4,7 @@ export const MovieCard = ({ movie }) => (
   <div className="card" data-cy="Movie">
     <div className="card-image">
       <figure className="image is-4by3">
-        <img data-cy="MovieImage" src={movie.imageUrl} alt="Film logo" />
+        <img data-cy="MovieImage" src={movie.imgUrl} alt="Film logo" />
       </figure>
     </div>
 
@@ -26,10 +26,7 @@ export const MovieCard = ({ movie }) => (
       <div className="content">
         <p data-cy="MovieDescription">{movie.description}</p>
 
-        <a
-          href={`https://www.imdb.com/title/${movie.imdbId}`}
-          data-cy="MovieLink"
-        >
+        <a href={movie.imdbUrl} data-cy="MovieLink">
           IMDB
         </a>
       </div>
