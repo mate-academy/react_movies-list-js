58b59cc2c5d9489c20624002bc5e54a284cb1819
Anastasiia Levochkina <nastya.mixaylova98@gmail.com>
2026-01-21 03:53:12 +0200

    add task solution

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,3 +1,29 @@
 import './MovieCard.scss';
 
-export const MovieCard = () => <>Put the card here</>;
+export const MovieCard = ({ movie }) => {
+  return (
+    <div className="card" data-cy="Movie">
+      <div className="card-image">
+        <figure className="image is-4by3">
+          <img data-cy="MovieImage" src={movie.imgUrl} alt={movie.title} />
+        </figure>
+      </div>
+
+      <div className="card-content">
+        <p className="title is-6" data-cy="MovieTitle">
+          {movie.title}
+        </p>
+
+        <p data-cy="MovieDescription">{movie.description}</p>
+        <a
+          href={movie.imdbUrl}
+          target="_blank"
+          rel="noreferrer"
+          data-cy="MovieLink"
+        >
+          IMDb
+        </a>
+      </div>
+    </div>
+  );
+};
