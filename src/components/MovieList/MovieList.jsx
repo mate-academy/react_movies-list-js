96d2adc830f7af09f319654fd3f1b176e787b503
shymkivvasyl <shymkiv.vasyl333@gmail.com>
2026-02-12 14:45:11 +0200

    Add another file with jsx

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -5,7 +5,7 @@ export const MovieList = ({ movies }) => {
   return (
     <div className="movies">
       {movies.map(movie => (
-        <MovieCard key={movie.imddId} movie={movie} />
+        <MovieCard key={movie.imdbId} movie={movie} />
       ))}
     </div>
   );
