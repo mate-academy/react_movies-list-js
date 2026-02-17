30e850e70d47078c22183241ca8e33d2d5f84636
Andriy <andriit35@gmail.com>
2026-02-17 14:01:20 +0200

    perfect solution

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,3 +1,18 @@
 import './MovieList.scss';
+import { MovieCard } from '../MovieCard/MovieCard';
 
-export const MovieList = () => <>Put the list here</>;
+export const MovieList = ({ movies }) => {
+  if (!movies || movies.length === 0) {
+    return null;
+  }
+
+  return (
+    <div className="page-content">
+      <div className="movies">
+        {movies.map(movie => (
+          <MovieCard movie={movie} key={movie.imdbId} />
+        ))}
+      </div>
+    </div>
+  );
+};
