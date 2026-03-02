a628ef51e43434c9b33983a729ace775955c7745
Volodymyr Shuper <vshuper@gmail.com>
2026-03-02 19:04:00 +0100

    add task solution

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -4,14 +4,10 @@ import './MovieList.scss';
 export const MovieList = ({ movies }) => (
   <div className="movies">
     {movies.map(movie => (
-      <div key={movie.imdbId} className="card" data-cy="Movie">
-        <div className="card-image">
-          <figure className="image is-4by3">
-            <img data-cy="MovieImage" src={movie.imgUrl} alt="Film logo" />
-          </figure>
-        </div>
-        <MovieCard movie={movie} />
-      </div>
+      <MovieCard
+        key={movie.imdbId}
+        movie={movie}
+      />
     ))}
   </div>
 );
