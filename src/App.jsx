19a475ccc149c70f7f3d7152b09cd540c1548e39
Yaroslav Grom <gromyarik3@gmail.com>
2026-03-04 11:22:05 +0100

    fix arhitecture

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,17 @@
 import React from 'react';
-import { MovieCard } from './components/MovieCard';
-import movies from './api/movies.json';
 import './App.scss';
 
-export const App = () => {
-  const firstMovie = movies[0];
-  const secondMovie = movies[1];
+import { MovieList } from './components/MovieList';
+import moviesFromServer from './api/movies.json';
 
-  return (
-    <div className="page">
-      <div className="page-content">
-        <div className="movies">
-          {movies.map(movie => (
-            <MovieCard movie={movie} key={movie.imdbId} />
-          ))}
-        </div>
-      </div>
-
-      <div className="sidebar" data-cy="Sidebar">
-        Sidebar will be here
-      </div>
+export const App = () => (
+  <div className="page">
+    <div className="page-content">
+      <MovieList movies={moviesFromServer} />
     </div>
-  );
-};
+
+    <div className="sidebar" data-cy="Sidebar">
+      Sidebar will be here
+    </div>
+  </div>
+);
