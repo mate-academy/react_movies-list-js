1fff6cee2c87b0ad0f31be1c88144e911fec0a5c
yevkrasnenkov <krasnenkov.evgen@gmail.com>
2026-04-12 00:28:20 -0400

    Id caps

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -5,7 +5,7 @@ export const MovieList = ({ movies }) => {
   return (
     <div className="movies">
       {movies.map(movie => (
-        <MovieCard key={movie.imdbid} movie={movie} />
+        <MovieCard key={movie.imdbId} movie={movie} />
       ))}
     </div>
   );
