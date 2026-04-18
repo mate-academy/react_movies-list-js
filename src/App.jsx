8c9635cac49f11634328919239f45049dc10ac9a
kennyprog <overbafer08@gmail.com>
2026-04-18 11:35:21 +0300

    changed fileroad

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 /* eslint-disable max-len */
 
 import './App.scss';
-import { MovieList } from './components/MovieList';
+import { MovieList } from './components/MovieList/MovieList';
 import moviesFromServer from './api/movies.json';
 
 export const App = () => (
   <div className="page">
     <div className="page-content">
-    <MovieList movies={moviesFromServer} />
+      <MovieList movies={moviesFromServer} />
     </div>
 
     <div className="sidebar" data-cy="Sidebar">
