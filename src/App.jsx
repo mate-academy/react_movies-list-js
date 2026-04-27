c5af6855db9ab33d8fe892157eb70790f5f2045b
Marcos Vinicius <marcos_araujo7@hotmail.com>
2026-04-27 14:05:35 -0300

    add key on map function

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable max-len */
 
-import './App.scss';
-import moviesFromServer from './api/movies.json';
-import { MovieList } from './components/MovieList/MovieList';
+import "./App.scss";
+import moviesFromServer from "./api/movies.json";
+import { MovieList } from "./components/MovieList/MovieList";
 
 export const App = () => (
   <div className="page">
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,10 +1,10 @@
-import { MovieCard } from '../MovieCard/MovieCard';
-import './MovieList.scss';
+import { MovieCard } from "../MovieCard/MovieCard";
+import "./MovieList.scss";
 
-export const MovieList = ({ movies }) => <>
+export const MovieList = ({ movies }) => (
   <div className="movies">
-    { movies.map(movie => (
-      <MovieCard movie={movie} />
-    )) }
+    {movies.map((movie) => (
+      <MovieCard movie={movie} key={movie.imdbId}/>
+    ))}
   </div>
-</>;
+);
