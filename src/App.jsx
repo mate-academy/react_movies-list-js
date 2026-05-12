3cb5d0337a466b923c9637a7977d5159e56a0f96
Cássia <cassiarsqueiroz@gmail.com>
2026-05-12 17:16:52 -0300

    Solution without changing index

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 /* eslint-disable max-len */
 
 import './App.scss';
+import moviesFromServer from './api/movies.json';
 import { MovieList } from './components/MovieList/MovieList';
 
-export const App = ({ moviesFromServer }) => (
+export const App = () => (
   <div className="page">
     <div className="page-content" />
     <MovieList movies={moviesFromServer} />
--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import 'bulma/css/bulma.css';
-import moviesFromServer from './api/movies.json';
 
 import { App } from './App';
 
-createRoot(document.getElementById('root')).render(
-  <App moviesFromServer={moviesFromServer} />,
-);
+createRoot(document.getElementById('root')).render(<App />);
