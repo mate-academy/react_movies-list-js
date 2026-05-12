ab0c691824319eb95d3b49168f3a4d1efe6654b2
Cássia <cassiarsqueiroz@gmail.com>
2026-05-11 22:59:12 -0300

    Solution

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,9 @@
 import { createRoot } from 'react-dom/client';
 import 'bulma/css/bulma.css';
+import moviesFromServer from './api/movies.json';
 
 import { App } from './App';
 
-createRoot(document.getElementById('root')).render(<App />);
+createRoot(document.getElementById('root')).render(
+  <App moviesFromServer={moviesFromServer} />,
+);
