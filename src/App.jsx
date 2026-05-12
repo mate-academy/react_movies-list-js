99ddc6471cbc6b3795706daa4aeeb0c61fd72a6a
Cássia <cassiarsqueiroz@gmail.com>
2026-05-12 16:19:40 -0300

    Solution improved

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 /* eslint-disable max-len */
 
 import './App.scss';
-import movies from './api/movies.json';
 import { MovieList } from './components/MovieList/MovieList';
 
-export const App = () => (
+export const App = ({ moviesFromServer }) => (
   <div className="page">
     <div className="page-content" />
-    <MovieList movies={movies} />
+    <MovieList movies={moviesFromServer} />
     <div className="sidebar" data-cy="Sidebar">
       Sidebar will be here
     </div>
