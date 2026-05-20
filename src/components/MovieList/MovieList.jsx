185dbf02c4c9d5bb05fd194b12552b647f8c9fff
Rustam Herheman <gergemannn@gmail.com>
2026-05-21 00:35:33 +0300

    add solution 3

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -5,7 +5,7 @@ import { MovieCard } from '../MovieCard';
 export const MovieList = ({ movies }) => (
   <div className="movies">
     {movies.map(movie => (
-      <MovieCard key={movie.imbdId} movie={movie} />
+      <MovieCard key={movie.imdbId} movie={movie} />
     ))}
   </div>
 );
