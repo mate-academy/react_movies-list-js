f2b702790edfff6fcc84ef84387201b9d69b09aa
Alibobick <dozenkoalex6@gmail.com>
2026-05-22 18:24:58 +0300

    add task solution

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,3 +1,12 @@
 import './MovieList.scss';
+import { MovieCard } from '../MovieCard';
 
-export const MovieList = () => <>Put the list here</>;
+export const MovieList = ({ movies }) => (
+  <div className="movies">
+    {movies.length === 0 ? (
+      <p>There are no movies</p>
+    ) : (
+      movies.map(movie => <MovieCard key={movie.imdbId} movie={movie} />)
+    )}
+  </div>
+);
