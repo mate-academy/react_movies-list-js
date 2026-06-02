0099a77a3b2eadcf26e76bc38b4d38163b507c60
PavloMolytovnyk <pavlomolytovny@icloud.com>
2026-06-02 13:32:41 +0300

    Change 1

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,37 @@
 import './App.scss';
 import { MovieList } from './components/MovieList/MovieList';
 import moviesData from './api/movies.json';
-// import moviesFromServer from './api/movies.json';
+import { useState } from 'react';
 
-export const App = () => (
-  <div className="page">
-    <div className="page-content">
-      <MovieList movies={moviesData} />
-    </div>
+function prepareMovies(goods, query) {
+  let sorted = [...goods];
+  let newQuery = query.toLowerCase();
 
-    <div className="sidebar" data-cy="Sidebar">
-      Sidebar will be here
+  sorted = sorted.filter(good => (
+    good.title.toLowerCase().includes(newQuery))
+    || (good.description.toLowerCase().includes(newQuery)));
+
+  return sorted;
+};
+
+export const App = () => {
+  const [sortField, setSortField] = useState('');
+  const visibleGoods = prepareMovies(moviesData, sortField);
+
+  return (
+    <div className="page">
+      <div className="page-content">
+        <input
+          type="text"
+          value={sortField}
+          onChange={(event) => (setSortField(event.target.value))} />
+
+        <MovieList movies={visibleGoods} />
+      </div>
+
+      <div className="sidebar" data-cy="Sidebar">
+        Sidebar will be here
+      </div>
     </div>
-  </div>
-);
+  )
+};
