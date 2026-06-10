ba6818aeb10d81ceb11c9088ae4cc13a60623fa1
Yurii Kondrashenko <droopy215421@gmail.com>
2026-06-10 14:34:52 +0200

    task completed

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,3 +1,18 @@
 import './MovieCard.scss';
 
-export const MovieCard = () => <>Put the card here</>;
+export const MovieCard = ({ movie }) => (
+  <div className="card" data-cy="Movie">
+    ...
+    <img data-cy="MovieImage" src={movie.imgUrl} alt="Film logo" />
+    ...
+    <p className="title is-8" data-cy="MovieTitle">
+      {movie.title}
+    </p>
+    ...
+    <p data-cy="MovieDescription">{movie.description}</p>
+    ...
+    <a href={movie.imdbUrl} data-cy="MovieLink">
+      IMDB
+    </a>
+  </div>
+);
