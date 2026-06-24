0fb82e0cd0c800c5793bd38564824fd11d064622
Maksym Savchenko <maks.20050817@gmail.com>
2026-06-24 19:59:00 +0200

    ready to check

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -4,7 +4,7 @@ import { MovieCard } from '../MovieCard';
 export const MovieList = ({ movies }) => (
   <div className="movies">
     {movies.map(movie => (
-      <MovieCard movie={movies} key={movie.imdbId} />
+      <MovieCard movie={movie} key={movie.imdbId} />
     ))}
   </div>
 );
