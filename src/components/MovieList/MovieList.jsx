d8b9df366f98d73b120cab0b9120b2656987452c
Ihor Balakhmei <balakhmei.dev@gmail.com>
2026-07-20 09:46:55 +0200

    add second task solution

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -4,7 +4,7 @@ import './MovieList.scss';
 export const MovieList = ({ movies }) => (
   <div className="movies">
     {movies.map(movie => (
-      <MovieCard movie={movie} key={movie.imbId} />
+      <MovieCard movie={movie} key={movie.imdbId} />
     ))}
   </div>
 );
