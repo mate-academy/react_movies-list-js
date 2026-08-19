6051b24784901d67d24d9324e67901cd79eafdce
Sonya <cuka9955@gmail.com>
2026-08-19 18:21:01 +0200

    add new solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable max-len */
 import './App.scss';
 import moviesFromServer from './api/movies.json';
-import { MovieList } from './components/MovieList/MovieList';
+import { MovieList } from './components/MovieList';
 
 export const App = () => (
   <div className="page">
