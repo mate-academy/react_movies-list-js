69e4c01b8e7140ff4cf3b6bc75b660d46de0e047
Vladyslav <vlad.lozynsky@gmail.com>
2026-08-25 12:33:23 +0300

    Problem fixed

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -12,7 +12,7 @@ export const MovieCard = ({ movie }) => (
       <div className="media">
         <div className="media-left">
           <figure className="image is-48x48">
-            <img src={movie.imdbUrl} alt="imdb" />
+            <img src="images/imdb-logo.jpeg" alt="imdb" />
           </figure>
         </div>
 
@@ -26,7 +26,7 @@ export const MovieCard = ({ movie }) => (
       <div className="content">
         <p data-cy="MovieDescription">{movie.description}</p>
 
-        <a href="https://www.imdb.com/title/tt1375666" data-cy="MovieLink">
+        <a href={movie.imdbUrl} data-cy="MovieLink">
           IMDB
         </a>
       </div>
