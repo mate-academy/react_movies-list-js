452b0a935a23d095c7a971a9f8dafcf2f702a90d
Yevhenii Dmytriienko <dmitrik24042007@gmail.com>
2026-08-25 19:59:19 +0300

    small update in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,12 @@ import moviesFromServer from './api/movies.json';
 import { MovieList } from './components/MovieList/MovieList';
 
 export const App = () => (
-  <>
-    <div className="page">
-      <div className="page-content">
-        <MovieList movies={moviesFromServer} />
-      </div>
+  <div className="page">
+    <div className="page-content">
+      <MovieList movies={moviesFromServer} />
     </div>
-
     <div className="sidebar" data-cy="Sidebar">
       Sidebar will be here
     </div>
-  </>
+  </div>
 );
