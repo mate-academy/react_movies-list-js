68cf34a2c4296f221542726dbc15910923dbf967
Olga Shcherbatiuk <nika32.n3@gmail.com>
2026-08-26 17:31:20 +0300

    Solution

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,3 +1,10 @@
 import './MovieList.scss';
+import { MovieCard } from '../MovieCard/MovieCard';
 
-export const MovieList = () => <>Put the list here</>;
+export const MovieList = ({ movies }) => {
+  const preparedMovies = movies.map(movie => (
+    <MovieCard key={movie.imdbId} movie={movie} />
+  ));
+
+  return <div className="movies">{preparedMovies}</div>;
+};
