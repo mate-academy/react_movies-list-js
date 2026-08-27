591a986d6f902491c00ef20a09c12a9642016c4f
Timofei <kiselovtimofej@gmail.com>
2026-08-27 18:38:14 +0300

    Add key to card

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -5,7 +5,7 @@ import { MovieCard } from '../MovieCard/MovieCard';
 export const MovieList = ({ movies }) => (
   <div className="movies">
     {movies.map(movie => (
-      <MovieCard movie={movie} />
+      <MovieCard key={movie.imdbId} movie={movie} />
     ))}
   </div>
 );
